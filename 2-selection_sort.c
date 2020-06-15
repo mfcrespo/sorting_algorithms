15a5fa94c3a4254576e2e8a0e111327a2bc44906
mfcrespo <mafe_crespo@hotmail.com>
2020-06-15 17:53:47 +0000

    Fix task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,25 +9,28 @@
 
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i = 0, j, min = 0, temp = 0;
+	int i, j, min, temp;
+	int new_size = size;
 
 	if (array == NULL || size == 0)
 		return;
 
-	for (; i < (size - 1); i++)
+	for (i = 0; i < (new_size - 1); i++)
 	{
 		min = i;
-		for (j = i + 1; j < size; j++)
+		for (j = i + 1; j < new_size; j++)
 		{
 			if (array[j] < array[min])
 			{
 				min = j;
 			}
 		}
-		if (array[min] != array[i])
-			temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
-			print_array(array, size);
+		if (min == i)
+			continue;
+
+		temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
+		print_array(array, size);
 	}
 }
